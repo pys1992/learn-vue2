63b6751c3a01a0d3a363af1e0fac7152e8d25306
psy <me@pys.im>
2021-10-24 19:16:52 +0800

    fix: method name

--- src/App.vue
+++ src/App.vue
@@ -55,7 +55,7 @@ export default {
             })
         },
 
-        handleFilterChangeMockhandleFilterChange() {
+        handleFilterChange() {
             this.fetchChartData()
         },
 
